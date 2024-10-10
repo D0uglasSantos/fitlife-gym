ccc590864239ec94c0592cfca79bf99ba44d8b74
D0uglasSantos <douglasas410@gmail.com>
2024-10-09 22:10:41 -0300

    :recycle: refactor: refactor bannerpages

--- src/components/BannerPages/index.tsx
+++ src/components/BannerPages/index.tsx
@@ -9,8 +9,8 @@ const BannerPages = ({ namePage }: BannerProps) => {
   return (
     <section className="bg-bannerImage bg-cover flex flex-col items-center lg:gap-8 lg:h-screen ">
       <Navbar />
-      <div className="flex justify-between w-4/5">
-        <div className="flex flex-col gap-8 w-[54%]">
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-8 w-full h-screen items-center justify-center">
           <h1 className="font-extrabold text-white text-6xl uppercase">
             {namePage}
           </h1>
